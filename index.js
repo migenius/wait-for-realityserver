28886c959c0da2ad1e2e940876a497a677423ffe
Paul Arden <paul@migenius.com>
2017-06-13 21:38:11 +1000

    Added support for RealityServer with UAC configured.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var request = require('request').defaults({jar: true});
 
 module.exports = function(host, port, options, successCallback, progressCallback) {
 
@@ -21,7 +21,7 @@ module.exports = function(host, port, options, successCallback, progressCallback
 	if (!(requestTimeout > 0)) throw new Error('Invalid value for option "requestTimeout"');
 
 	// RealityServer JSON-RPC 2.0 request to obtain version number
-	var body = {
+	var command = {
 		jsonrpc: 2.0,
 		method: "get_version",
 		params: {},
@@ -30,23 +30,55 @@ module.exports = function(host, port, options, successCallback, progressCallback
 
 	// Attempt to get the RealityServer version and retry on failure
 	function tryToConnect() {
-		request({
-			method: 'POST',
-			uri: 'http://' + host + ":" + port + "/",
-			json: body,
-			timeout: requestTimeout
+		request({ // Attempt to make UAC session first
+			method: 'GET',
+			uri: 'http://' + host + ":" + port + "/uac/create/",
+			timeout: requestTimeout					
 		}, function(error, response, body) {
-			if(error || typeof body.result !== 'string') {
+			if (error) {
 				typeof progressCallback === 'function' && progressCallback({numRetries: numRetries, retriesRemaining: retriesRemaining, retryInterval: retryInterval});
-				if (--retriesRemaining < 0) return successCallback(new Error('Retry limit reached, RealityServer not available'));
+				if (--retriesRemaining < 0)
+					return successCallback(new Error('Retry limit reached, RealityServer not available'));
 				setTimeout(tryToConnect, retryInterval);
 				return;				
 			}
-			if (retriesRemaining > 0) successCallback(null, {version: body.result});
+			if (retriesRemaining > 0) {
+				request({
+					method: 'POST',
+					uri: 'http://' + host + ":" + port + "/",
+					json: command,
+					timeout: requestTimeout					
+				}, function(error, response, versionBody) {
+					if (error || typeof versionBody.result !== 'string') {
+						typeof progressCallback === 'function' && progressCallback({numRetries: numRetries, retriesRemaining: retriesRemaining, retryInterval: retryInterval});
+						if (--retriesRemaining < 0)
+							return successCallback(new Error('Retry limit reached, RealityServer not available'));
+						setTimeout(tryToConnect, retryInterval);
+						return;				
+					}
+					if (retriesRemaining > 0) {
+						request({
+							method: 'GET',
+							uri: 'http://' + host + ":" + port + "/uac/destroy/",
+							timeout: requestTimeout					
+						}, function(error, response, body) {
+							if (error) {
+								typeof progressCallback === 'function' && progressCallback({numRetries: numRetries, retriesRemaining: retriesRemaining, retryInterval: retryInterval});
+								if (--retriesRemaining < 0)
+									return successCallback(new Error('Retry limit reached, RealityServer not available'));
+								setTimeout(tryToConnect, retryInterval);
+								return;				
+							}
+							if (retriesRemaining > 0) {
+								successCallback(null, {version: versionBody.result});
+							}
+						});
+					}
+				});
+			}
 		});
 	}
 
 	// Kick off the process
 	tryToConnect();
-
 };
